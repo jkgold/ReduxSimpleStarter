26f101503e436ff73795ccbd648fcd04e1b55142
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2017-06-06 13:34:08 -0600

    added event handling of the target and console log of what user is typing

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />;
+class SearchBar extends React.Component {
+  render() {
+    return <input onChange={event => console.log(event.target.value)} />;
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
 }
+
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY ='AIzaSyCbxqg1Jbzpor23IYRJGQIuLm9x9vDIpF8';
 
 // create new component should produce html
 const App = () => {
-  return <div>HI!</div>;
-}
+  return (
+        <div>
+          <SearchBar />
+        </div>
+      );
+  }
+
 
 // take component and put it on the page (in DOM)
 
