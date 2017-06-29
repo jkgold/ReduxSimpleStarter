4f78b75bf6dd6ca8113b2df10e3db604e435e3ea
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2017-06-29 16:41:51 -0600

    video detail module started page still functional

---
+++ src/components/video_detail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const VideoDetail = (props)  => {
+  return (
+    <div className="video-detail col-md">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-response-item"></iframe>
+      </div>
+      <div className="details">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+
+};
+
+export default VideoDetail;
