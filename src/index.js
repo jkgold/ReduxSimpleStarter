cff9f1abab1e0b53378d041cebce2a869b4754ca
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2017-06-30 14:40:01 -0600

    now the list renders and video title and description are defined and can play audiobook

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY ='AIzaSyCbxqg1Jbzpor23IYRJGQIuLm9x9vDIpF8';
 
@@ -15,12 +16,14 @@ class App extends Component {
 
     YTSearch({key: API_KEY, term: 'audiobooks'}, (videos) => {
       this.setState({ videos });
+      console.log({videos});
     });
   }
   render() {
   return (
         <div>
           <SearchBar />
+          <VideoDetail video={this.state.videos[0]} />
           <VideoList videos={this.state.videos} />
         </div>
       );
